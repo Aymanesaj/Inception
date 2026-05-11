209ada6a2980de53c6533818f8d05a217dbc2cd4
Oracle Public Cloud User <opc@my-vnic.mysubnet.myvcn.oraclevcn.com>
2026-05-11 01:49:55 +0000

    changed nginx config, entrypoint script and dockerfile to generate ssl the certificate and private key

--- srcs/requirements/nginx/tools/entrypoint.sh
+++ srcs/requirements/nginx/tools/entrypoint.sh
@@ -1,3 +1,12 @@
 #!/bin/sh
 
+if [ ! -f /etc/nginx/ssl/server.crt ] || [ ! -f /etc/nginx/ssl/server.key ]; then
+  echo "Generating self-signed certificate..."
+  openssl req -x509 -nodes -days 365 \
+    -newkey rsa:2048 \
+    -keyout /etc/nginx/ssl/server.key \
+    -out /etc/nginx/ssl/server.crt \
+    -subj "/C=US/ST=NA/L=NA/O=Local/OU=Dev/CN=localhost"
+fi
+
 exec nginx -g "daemon off;"
